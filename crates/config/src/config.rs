e8da1d2137cd479580538e2e88958294a65d935f
DiCesareDaniele <dicesaredaniele7@gmail.com>
2024-11-19 12:27:16 +0100

    features serialize optional

--- crates/config/src/config.rs
+++ crates/config/src/config.rs
@@ -1,25 +1,30 @@
+#[cfg(feature = "serialize")]
 use serde::Deserialize;
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug)]
+#[cfg_attr(feature = "serialize", derive(Deserialize))]
 pub struct Drone {
     pub id: u64,
     pub connected_drone_ids: Vec<u64>,
     pub pdr: f64,
 }
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug)]
+#[cfg_attr(feature = "serialize", derive(Deserialize))]
 pub struct Client {
     pub id: u64,
     pub connected_drone_ids: Vec<u64>,
 }
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug)]
+#[cfg_attr(feature = "serialize", derive(Deserialize))]
 pub struct Server {
     pub id: u64,
     pub connected_drone_ids: Vec<u64>,
 }
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug)]
+#[cfg_attr(feature = "serialize", derive(Deserialize))]
 pub struct Config {
     pub drone: Vec<Drone>,
     pub client: Vec<Client>,
