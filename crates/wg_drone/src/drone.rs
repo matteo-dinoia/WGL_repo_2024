39f0b10947fe65fda2799c91495c4c1fe568ca04
Gino <60258854+ZGako@users.noreply.github.com>
2024-11-25 10:47:18 +0100

    added packet_send channels in drone new() function (#57)
    * added packet_send channels in drone init
    also changed drone_usage example to be more accurate
    
    * ran cargo fmt to fix formatting error
    
    ---------
    
    Co-authored-by: Matteo <dmatteo002@gmail.com>

--- crates/wg_drone/src/drone.rs
+++ crates/wg_drone/src/drone.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use crossbeam_channel::{Receiver, Sender};
 use wg_controller::Command;
 use wg_network::NodeId;
@@ -9,6 +11,7 @@ pub struct DroneOptions {
     pub sim_contr_send: Sender<Command>,
     pub sim_contr_recv: Receiver<Command>,
     pub packet_recv: Receiver<Packet>,
+    pub packet_send: HashMap<NodeId, Sender<Packet>>,
     pub pdr: f32,
 }
 
